83ea3f20d4f5cb50a4817cdc440662eb42a3d65d
kim min suk <rocker44@naver.com>
2018-11-26 21:51:59 +0900

    Update example_cancel.php

--- example/example_cancel.php
+++ example/example_cancel.php
@@ -3,15 +3,15 @@ require_once(dirname(__DIR__).'/src/iamport.php');
 
 $iamport = new Iamport('YOUR_IMP_REST_API_KEY', 'YOUR_IMP_REST_API_SECRET');
 
-#3. 주문취소
+#3. Cancel payment (주문취소)
 $result = $iamport->cancel(array(
-	'imp_uid'	=> '거래 건의 imp_uid', 		//merchant_uid에 우선한다
-	'merchant_uid'	=> '거래 건의 merchant_uid', 	//imp_uid 또는 merchant_uid가 지정되어야 함
-	'amount' 	=> 1000,					//amount가 생략되거나 0이면 전액취소. 금액지정이면 부분취소(PG사 정책별, 결제수단별로 부분취소가 불가능한 경우도 있음)
-	'reason'	=> '취소테스트',				//취소사유
-	'refund_holder' => '환불될 가상계좌 예금주', 		//이용 중인 PG사에서 가상계좌 환불 기능을 제공하는 경우. 일반적으로 특약 계약이 필요
-	'refund_bank'	=> '환불될 가상계좌 은행코드',
-	'refund_account'=> '환불될 가상계좌 번호'
+	'imp_uid'	=> 'iamport ID: imp_uid (거래 건의 imp_uid)', 				//merchant_uid에 우선한다
+	'merchant_uid'	=> ' Product ID: merchant_uid (거래 건의 merchant_uid)', 			//imp_uid 또는 merchant_uid가 지정되어야 함
+	'amount' 	=> 1000,								   //amount가 생략되거나 0이면 전액취소. 금액지정이면 부분취소(PG사 정책별, 결제수단별로 부분취소가 불가능한 경우도 있음)
+	'reason'	=> 'Reason for cancellation (취소테스트)',				       //취소사유
+	'refund_holder' => 'Virtual account holder to be refunded (환불될 가상계좌 예금주)', 	   //이용 중인 PG사에서 가상계좌 환불 기능을 제공하는 경우. 일반적으로 특약 계약이 필요
+	'refund_bank'	=> 'Virtual account bank code to be refunded (환불될 가상계좌 은행코드)',
+	'refund_account'=> 'Virtual account number to be refunded (환불될 가상계좌 번호)'
 ));
 if ( $result->success ) {
 	/**
@@ -20,13 +20,13 @@ if ( $result->success ) {
 	*/
 	$payment_data = $result->data;
 
-	echo '## 취소후 결제정보 출력 ##';
-	echo '결제상태 : ' 		. $payment_data->status;
-	echo '결제금액 : ' 		. $payment_data->amount;
-	echo '취소금액 : ' 		. $payment_data->cancel_amount;
-	echo '결제수단 : ' 		. $payment_data->pay_method;
-	echo '결제된 카드사명 : ' 	. $payment_data->card_name;
-	echo '결제(취소) 매출전표 링크 : '	. $payment_data->receipt_url;
+	echo '## Print billing information after cancellation (취소후 결제정보 출력) ##';
+	echo 'Payment status(결제상태) : ' 		. $payment_data->status;
+	echo 'Amount of payment(결제금액) : ' 		. $payment_data->amount;
+	echo 'Cancellation amount(취소금액) : ' 		. $payment_data->cancel_amount;
+	echo 'method of payment(결제수단) : ' 		. $payment_data->pay_method;
+	echo 'Name of credit card company paid (결제된 카드사명) : ' 	. $payment_data->card_name;
+	echo 'Billing (canceled) Sales slip link (결제(취소) 매출전표 링크) : '	. $payment_data->receipt_url;
 	//등등 __get을 선언해 놓고 있어 API의 Payment Model의 값들을 모두 property처럼 접근할 수 있습니다.
 } else {
 	error_log($result->error['code']);
